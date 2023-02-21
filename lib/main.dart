a7d969b142405615956973f242a7c5da6985370d
kenway23 <rifkimuhamad6635@gmail.com>
2023-02-21 08:58:21 +0700

    Push App

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Latihan Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 248, 16, 16),
+          centerTitle: true,
+          title:  Text("Latihan"),
+        ), 
+          body: Center(
+            child: Text("Hallo Dunia", style: TextStyle(fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+            backgroundColor: Colors.black12),) 
+            ),
+        ),
+    ) ;
+  }
+}
